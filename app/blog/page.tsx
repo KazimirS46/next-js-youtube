f18ab361c532e78515dbd75b663acf8e2b43205d
Kazimir <KazimirS46@gmail.com>
2023-06-11 15:12:50 +0400

    Getting posts using useeffect

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,35 +1,28 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import { Metadata } from 'next';
-import Link from 'next/link';
-
-async function getData() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
-    next: {
-      revalidate: 60,
-    },
-  });
-
-  if (!res.ok) throw new Error('Unable to fetch posts');
-
-  return res.json();
-}
+import { Posts } from '@/components/Posts';
+import { getAllPosts } from '@/services/getPosts';
 
 export const metadata: Metadata = {
   title: 'Next App Blogs',
 };
 
-export default async function Blog() {
-  const posts = await getData();
+export default function Blog() {
+  const [posts, setPosts] = useState<any[]>([]);
+  const [loading, setLoading] = useState<Boolean>(true);
+
+  useEffect(() => {
+    getAllPosts()
+      .then(setPosts)
+      .finally(() => setLoading(false));
+  }, []);
 
   return (
     <>
       <h1>Blog</h1>
-      <ul className='postList'>
-        {posts.map((post: any) => (
-          <li className='postLink' key={post.id}>
-            <Link href={`/blog/${post.id}`}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
+      {loading ? <h3>Loading...</h3> : <Posts posts={posts} />}
     </>
   );
 }
